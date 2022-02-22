c3826fb2e2369b86b24b162db5b5407167bd36ac
arosh-segar <arosh.segar@gmail.com>
2022-02-23 01:14:58 +0530

    changes made

--- app/functions/tesseract.py
+++ app/functions/tesseract.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 
 def imgToText(image):
-    pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract' 
+    pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
     im = Image.open(image)
     ocr_result = pytesseract.image_to_string(im)
     return ocr_result
